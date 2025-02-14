423f3c5e42ad549c0a17c8dd6d7a3dea6a218a6b
Valdeir Nascimento <valdeir.nascimento@fazenda.mg.gov.br>
2025-02-14 20:35:03 -0300

    Criação do projeto

---
+++ src/main/java/io/github/biblioteca_digital_api/BibliotecaDigitalApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.biblioteca_digital_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaDigitalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaDigitalApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/biblioteca_digital_api/BibliotecaDigitalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.biblioteca_digital_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaDigitalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
