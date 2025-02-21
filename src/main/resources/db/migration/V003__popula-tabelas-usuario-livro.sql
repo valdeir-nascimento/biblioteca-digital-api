d1f23c7abf60cad4a6e7f785c1f134add06aabfe
Valdeir Nascimento <valdeir.nascimento@fazenda.mg.gov.br>
2025-02-20 22:11:43 -0300

    refactor: Ajustes em massa de testes

--- src/main/resources/db/migration/V003__popula-tabelas-usuario-livro.sql
+++ src/main/resources/db/migration/V003__popula-tabelas-usuario-livro.sql
@@ -7,15 +7,15 @@ VALUES
 
 INSERT INTO livro (titulo, autor, disponivel, usuario_id, data_aluguel)
 VALUES
-  ('Clean Code', 'Robert C. Martin', TRUE, NULL, NULL),
+  ('Clean Code', 'Robert C. Martin', TRUE, 1, NULL),
   ('The Pragmatic Programmer', 'Andrew Hunt and David Thomas', FALSE, 1, CURRENT_TIMESTAMP),
-  ('Refactoring', 'Martin Fowler', TRUE, NULL, NULL),
+  ('Refactoring', 'Martin Fowler', TRUE, 1, NULL),
   ('Design Patterns: Elements of Reusable Object-Oriented Software', 'Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides', FALSE, 2, CURRENT_TIMESTAMP),
-  ('Domain-Driven Design', 'Eric Evans', TRUE, NULL, NULL),
+  ('Domain-Driven Design', 'Eric Evans', TRUE, 1, NULL),
   ('Working Effectively with Legacy Code', 'Michael Feathers', FALSE, 3, CURRENT_TIMESTAMP),
-  ('Continuous Delivery', 'Jez Humble and David Farley', TRUE, NULL, NULL),
+  ('Continuous Delivery', 'Jez Humble and David Farley', TRUE, 1, NULL),
   ('Test-Driven Development: By Example', 'Kent Beck', FALSE, 4, CURRENT_TIMESTAMP),
-  ('Patterns of Enterprise Application Architecture', 'Martin Fowler', TRUE, NULL, NULL),
+  ('Patterns of Enterprise Application Architecture', 'Martin Fowler', TRUE, 1, NULL),
   ('Clean Architecture', 'Robert C. Martin', FALSE, 1, CURRENT_TIMESTAMP),
-  ('The Clean Coder', 'Robert C. Martin', TRUE, NULL, NULL),
+  ('The Clean Coder', 'Robert C. Martin', TRUE, 1, NULL),
   ('Peopleware: Productive Projects and Teams', 'Tom DeMarco and Timothy Lister', FALSE, 2, CURRENT_TIMESTAMP);
