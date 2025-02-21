1b9db29ad2ea15313ff05ef0aa6f0cc9279bd78a
Valdeir Nascimento <valdeir.nascimento@fazenda.mg.gov.br>
2025-02-20 22:13:53 -0300

    feat: Bean de aluguel de livros

--- src/main/java/io/github/biblioteca/digital/api/application/adapter/config/BookConfig.java
+++ src/main/java/io/github/biblioteca/digital/api/application/adapter/config/BookConfig.java
@@ -1,8 +1,12 @@
 package io.github.biblioteca.digital.api.application.adapter.config;
 
+import io.github.biblioteca.digital.api.common.mapper.BookMapper;
+import io.github.biblioteca.digital.api.domain.port.in.BookRentalUseCasePort;
 import io.github.biblioteca.digital.api.domain.port.in.BookUseCasePort;
 import io.github.biblioteca.digital.api.domain.port.in.UserValidationUseCasePort;
 import io.github.biblioteca.digital.api.domain.port.out.BookRepositoryPort;
+import io.github.biblioteca.digital.api.domain.port.out.NotificationPort;
+import io.github.biblioteca.digital.api.domain.usecase.BookRentalUseCase;
 import io.github.biblioteca.digital.api.domain.usecase.BookUseCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +17,11 @@ public class BookConfig {
     @Bean
     public BookUseCasePort bookUseCase(BookRepositoryPort bookRepositoryPort, UserValidationUseCasePort validationUseCasePort) {
         return new BookUseCase(bookRepositoryPort, validationUseCasePort);
+    }
+
+    @Bean
+    public BookRentalUseCasePort bookRentalUseCase(BookUseCasePort bookUseCasePort, BookMapper bookMapper, NotificationPort notificationPort) {
+        return new BookRentalUseCase(bookUseCasePort, bookMapper, notificationPort);
     }
 
 }
